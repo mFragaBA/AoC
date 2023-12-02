185c4ffefd9f8a040dccf760ec9da97c296d3ec6
Martin Fraga <martin.fraga@lambdaclass.com>
2023-12-02 03:32:43 -0300

    add comments'

--- 2023/d1/d1.sh
+++ 2023/d1/d1.sh
@@ -1,1 +1,5 @@
+# First removes all non numeric characters, then we duplicate each row so we
+# can get via regexp the first and last characters and only keep those (the
+# duplication is because there might be lines with a single number). Lastly,
+# pipe the results into bc
 cat $1 | sed 's/[^0-9]*//g' | sed 's/\(.*\)/\1\1/g' | sed 's/\([0-9]\).*\([0-9]\)/\1\2/g' | paste -sd+ - | bc
